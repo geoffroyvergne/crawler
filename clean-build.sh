7ef19d9a029b8f22230486ea2efbb3d7692351dc
Geofroy Vergne <geoffroy.vergne@gmail.com>
2020-10-11 21:49:06 +0200

    updates

--- clean-build.sh
+++ clean-build.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+#export CC=/usr/local/bin/clang10
+#export CXX=/usr/local/bin/clang++10
+
 rm -Rf build/
 
 mkdir build && \
-conan install --build=missing . -if build/ && \
+conan install . -if build/ && \
+#conan install --build=missing . -if build/ && \
 cmake . -B build/ && \
+#cmake . -DCMAKE_CXX_FLAGS="-pthread" -DCMAKE_CXX_COMPILER=/usr/local/bin/clang++10 -B build/
 cmake --build build/
