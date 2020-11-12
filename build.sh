0a09ceb56a379464194f402a4634724d4170cd96
Geofroy Vergne <geoffroy.vergne@gmail.com>
2020-11-12 19:28:58 +0100

    updates

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Linux
+#export CC=/usr/bin/clang-9
+#export CXX=/usr/bin/clang++-9
+
+# Freebsd
+#export CC=/usr/local/bin/clang90
+#export CXX=/usr/local/bin/clang++90
+
 #cmake . -B build/
 cmake --build build/
 
--- clean-build.sh
+++ clean-build.sh
@@ -4,6 +4,7 @@
 #export CC=/usr/bin/clang-9
 #export CXX=/usr/bin/clang++-9
 
+# Freebsd
 #export CC=/usr/local/bin/clang90
 #export CXX=/usr/local/bin/clang++90
 
